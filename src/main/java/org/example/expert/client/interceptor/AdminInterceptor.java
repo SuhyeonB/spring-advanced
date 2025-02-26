5975306d632a1843bc27fa165773f5502ce3e546
peridot <peridot489@gmail.com>
2025-02-27 04:07:42 +0900

    level 4: Interceptor (revised)

--- src/main/java/org/example/expert/client/interceptor/AdminInterceptor.java
+++ src/main/java/org/example/expert/client/interceptor/AdminInterceptor.java
@@ -1,14 +1,16 @@
-package org.example.expert.domain.common.interceptor;
+package org.example.expert.client.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.user.enums.UserRole;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
+
 
 @Slf4j
 @Component
@@ -17,11 +19,20 @@ public class AdminInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandler");
         // 요청 전
         // HttpServletRequest를 사전처리 어드민 권한 여부 확인, 인증 되지 않은 사용자 접근 차단 (Filter에서 Jwt 검증)
-        UserRole userRole = (UserRole) request.getAttribute("userRole");
-        if (userRole == null) return false;
-        return (userRole == UserRole.ADMIN) ? true : false;
+        UserRole userRole = (request.getAttribute("userRole").equals("ADMIN")) ? UserRole.ADMIN : UserRole.USER;
+        log.info("userRole: {}",userRole);
+        if (userRole == null) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "삭제 불가");
+            return false;
+        }
+        if (userRole.equals(UserRole.USER)) {
+            log.info("접근 차단 시간: {}", LocalDateTime.now());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자만이 접근할 수 있습니다.");
+        }
+        return true;
     }
 
     @Override
@@ -29,12 +40,14 @@ public class AdminInterceptor implements HandlerInterceptor {
         // 컨트롤러 실행 후
         // 어드민 접근 가능 로직 실행
 
-        String requestURI = request.getRequestURI();
+        // handler = Controller method
+        // chosen handler to execute, for type and/or instance evaluation
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        String methodName = handlerMethod.getMethod().getName();
 
         // deleteComment() & changeUserRole()
-        if (requestURI.equals("/admin/comment/delete") || requestURI.equals("/admin/user/changeRole")) {
-            log.info("관리자 접근 - 요청 시각: {}, 요청 URL: {}", System.currentTimeMillis(), requestURI);
-
+        if("deleteComment".equals(methodName) || "changeUserRole".equals(methodName)) {
+            log.info("관리자 접근 - 요청 시각: {}, 컨트롤러 메서드: {}", System.currentTimeMillis(), methodName);
         }
     }
 
--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -1,8 +1,11 @@
 package org.example.expert.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.client.interceptor.AdminInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,9 +14,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private final AdminInterceptor adminInterceptor;
+
     // ArgumentResolver 등록
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/admin/**");
     }
 }
